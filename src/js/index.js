0376d4e55e49960ef8fdfd61be1a81789753f9d4
Angela <angela.inniss@justpark.com>
2021-01-13 14:01:01 +0000

    add wiser test code

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,25 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("ready");
 });
+
+document.getElementById("desc-btn-purple")
+    .addEventListener("click", () => {
+      toggleStates()
+    });
+
+function toggleStates() {
+  const purpleBg = document.getElementById("container-purple");
+  const blackBg = document.getElementById("container-black");
+
+  purpleBg.style.display = (
+      purpleBg.style.display === "block" ? "none" : "block");
+
+  blackBg.style.display = (
+      blackBg.style.display === "none" ? "block" : "none");
+}
+
+document.getElementById("desc-btn-black")
+    .addEventListener("click", () => {
+      toggleStates();
+    });
+
